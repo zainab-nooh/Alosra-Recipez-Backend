276f68c7c5fd425011b89eae530da90aa639eff1
zainab-nooh <zain3b.nooh@gmail.com>
2025-09-16 21:50:24 +0300

    Add Cart Model

---
+++ models/cart.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column , Integer, String, ForeignKey,  DECIMAL, Text, DateTime, UniqueConstraint 
+from sqlalchemy.orm import relationship
+from .base import BaseModel
+
+
+class CartItem(BaseModel):
+    __tablename__ = "cart_items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False, index=True)
+    recipe_id = Column(Integer, ForeignKey("recipes.id"), nullable=False, index=True)
+    number_of_people = Column(Integer, nullable=False, default=1)
+
+
+    # Relationships
+
+    user = relationship("User", back_populates="cart_items")
+    recipe = relationship("Recipe", back_populates="cart_items")
+
+
+    # CONSTRAINTS
+    __table_args__ = (UniqueConstraint('user_id', 'recipe_id', name='unique_user_recipe_cart'),)
